a7f47b222b57e9a07f39985ef53a527e19f165ec
namgivu <namgivu@gmail.com>
2017-05-12 14:09:22 +0700

    install chrome 59+

--- util/common.sh
+++ util/common.sh
@@ -5,3 +5,10 @@ HL='\033[1;33m' #high-lighted color
 CM='\033[0;32m' #comment color
 ER='\033[1;31m' #error color
 EC='\033[0m'    #end coloring
+
+#region chrome path
+  chrome_beta='google-chrome-beta'
+chrome_stable='google-chrome-stable'
+     chrome59=$chrome_beta
+       chrome=$chrome_beta
+#endregion chrome path
