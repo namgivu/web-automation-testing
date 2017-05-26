eedb010327ba0e005fd375ea2c8a1391833a5078
namgivu <namgivu@gmail.com>
2017-05-26 18:54:18 +0700

    proboscis @ group dependency

---
+++ util/sandbox/proboscis-handon/test_vault/test02.py
@@ -0,0 +1,28 @@
+from proboscis.asserts import assert_true
+from proboscis import test
+
+
+@test(groups=["service.initialization"])
+def service_initialize():
+  """service_initialize"""
+
+
+@test(groups=["user", "user.initialization"],
+      depends_on_groups=["service.initialization"])
+def create_user():
+  """Create new user"""
+  #assert_true(1+1==22) #NOTE: If this one is turned on, its dependant e.g. group 'user.test' won't run
+
+
+@test(groups=["user", "user.test"],
+      depends_on_groups=["user.initialization"])
+def a_failOnPurpose_dummy():
+  """a_failOnPurpose_dummy"""
+  assert_true(1+1==333)
+
+
+@test(groups=["service.shutdown"],
+      depends_on_groups=["user.test"],
+      always_run=True)
+def shut_down():
+  """Shut down service"""
