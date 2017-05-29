f370593ae28be311010626cab01948740004c2a2
namgivu <namgivu@gmail.com>
2017-05-29 16:04:04 +0700

    chrome alias moved

--- util/common.sh
+++ util/common.sh
@@ -9,17 +9,3 @@ EC='\033[0m'    #end coloring
 
 #path
 BASHRC="$HOME/.bashrc"
-
-
-#region chrome alias
-alias   chrome_beta='google-chrome-beta'
-alias      chrome59='google-chrome-beta'
-alias        chrome='google-chrome-beta' #chromium 59+ alias as 'chrome'
-alias chrome_stable='google-chrome-stable'
-
-shopt -s expand_aliases #make bash script recognize alias ref. https://unix.stackexchange.com/a/1498/17671
-
-echo -e "${CM}Alias for chromium 59+ created${EC}"
-type chrome #aftermath check by printing alias 'chrome'
-echo
-#endregion chrome alias
--- util/install-ubuntu-16-chromium-59+/s02z_install-chrome-59+.sh
+++ util/install-ubuntu-16-chromium-59+/s02z_install-chrome-59+.sh
@@ -14,3 +14,18 @@
 
 #do install
 "$SCRIPT_HOME/s02b_install-chrome-beta.sh"
+
+
+
+#region chrome alias
+alias   chrome_beta='google-chrome-beta'
+alias      chrome59='google-chrome-beta'
+alias        chrome='google-chrome-beta' #chromium 59+ alias as 'chrome'
+alias chrome_stable='google-chrome-stable'
+
+shopt -s expand_aliases #make bash script recognize alias ref. https://unix.stackexchange.com/a/1498/17671
+
+echo -e "${CM}Alias for chromium 59+ created${EC}"
+type chrome #aftermath check by printing alias 'chrome'
+echo
+#endregion chrome alias
