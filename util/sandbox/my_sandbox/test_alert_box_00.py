4544109c7680ff6a31a317d54483f518a72096bf
namgivu <namgivu@gmail.com>
2017-06-21 11:39:53 +0700

    js alert sandbox @ show the box right when started

---
+++ util/sandbox/my_sandbox/test_alert_box_00.py
@@ -0,0 +1,38 @@
+from common import *
+from selenium.webdriver.support.ui import WebDriverWait          #available since 2.4.0
+from selenium.webdriver.support import expected_conditions as EC #available since 2.26.0
+
+"""Test alert box"""
+
+##region prepare the html file
+html='''
+<!DOCTYPE html>
+<html>
+<body>
+
+<p>An alert box must show on start-up</p>
+
+<script>
+alert("Hello! I am an alert box!");
+</script>
+
+</body>
+</html>
+'''
+
+#generate temp file
+import tempfile
+htmlFile = tempfile.NamedTemporaryFile().name #ref. https://stackoverflow.com/a/13717435/248616
+
+#write html to file ref. https://stackoverflow.com/a/27708256/248616
+with open(htmlFile, 'w') as f:
+  f.write(html)
+##endregion prepare the html file
+
+#open sample alert page
+driver = loadWebDriverCHROME()
+driver.get('file:///'+htmlFile)
+
+WebDriverWait(driver,10).until(EC.alert_is_present()) #TODO Why alert box not available? We got error here
+alertBox = driver.switch_to_alert()
+alertBox.accept()
