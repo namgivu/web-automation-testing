1e840d22937f06bc3c8308ad0313da584e22a346
Engineer Intern <engineer-intern@gigacover.com>
2019-11-16 17:22:39 +0700

    test with xpath and snapshot

--- test/test_00.py
+++ test/test_00.py
@@ -1,7 +1,8 @@
 import unittest
 
 from src.config import WEBDRIVER_REMOTE_HUB
-from src.service.selenium_.webdriver import loadWebDriver
+from src.service.selenium_.snapshot_debug import takeSnapshot
+from src.service.selenium_.webdriver import loadWebDriver, wait4VisibleXPath
 from src.service.selenium_.window_size import WindowSize
 
 
@@ -16,8 +17,18 @@ class Test(unittest.TestCase):
 
     def tearDown(self): pass  # nothing here for now
 
-    def test_homepage(self):
+
+    def test_apkathon2019_homepage(self):
         url = 'https://aptechvietnam.com.vn/apkathon2019/'
         self.wd.get(url)
         title = self.wd.title
         assert title=='Cuộc thi Apkathon 2019 - Hệ thống Đào tạo Lập trình viên Quốc tế Aptech'
+
+
+    def test_simple_google_search(self):
+        url = 'https://www.google.com'
+        self.wd.get(url)
+
+        # locate logo and take snapshot and stored under ./src/_snapshot_/vault/
+        x  = '//*[@id="hplogo"]'
+        _ = wait4VisibleXPath(self.wd, x); takeSnapshot(self.wd, printOutcome=True, suffix='snap0', forceSnapshot=True)
