c0de2dfba1b47dd068790101e908660fea4dd1cd
Engineer Intern <engineer-intern@gigacover.com>
2019-11-18 13:25:15 +0700

    update login_fb

--- src/scenario/login_fb.py
+++ src/scenario/login_fb.py
@@ -1,30 +1,29 @@
+from random import randint
+from time import sleep
+
 from src.service.selenium_.snapshot_debug import *
 from src.service.selenium_.webdriver import *
 from src.service.selenium_.window_size import *
 from src.config import WEBDRIVER_REMOTE_HUB
-from time import sleep
+
+def human_wise():
+    sleep(randint(3, 6))
 
 url = 'https://www.facebook.com/aptechvietnam.hcm'
 username = 'vippro56@yahoo.com.vn'
 password = 'matkhau1'
 wd = loadWebDriver(WEBDRIVER_REMOTE_HUB, WindowSize.PC)
 
-#TODO Trang 1 create human_wise() to sleep() in random
-#TODO Trang 2 add comment at each step to clarify what it is for
-
 wd.get(url)
-sleep(3)
-
-#region TODO Trang 2a login facebook
+human_wise()
+#region input username and password
 username_box = wd.find_element_by_id("email").send_keys(username)
-sleep(3)
-
+human_wise()
 password_box = wd.find_element_by_id("pass").send_keys(password)
-sleep(3)
+human_wise()
 #endregion
 
-
-#TODO Trang 2x what is this step for?
+# click login button
 login_btn = wd.find_element_by_xpath('/html/body/div[1]/div[2]/div/div/div/div/div[2]/form/table/tbody/tr[2]/td[3]/label/input').click()
-sleep(3)
+human_wise()
 takeSnapshot(wd, printOutcome=True, suffix='snap0', forceSnapshot=True)
