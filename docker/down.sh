97cd49134f5b818cc2d2cbe9eeb08354753703fc
namgivu <namgivu@gmail.com>
2020-05-05 16:39:00 +0700

    review docker up

--- docker/down.sh
+++ docker/down.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
+#                              q aka show only container id, a aka show all
 seleniumContainers="docker ps -qa
                               -f name=selenium-hub
                               -f name=selenium-node-ch
                               -f name=selenium-node-ff
 "
-sh="docker rm -f \$($seleniumContainers)"
-echo "$sh"
-eval $sh
+docker stop    $($seleniumContainers)
+docker rm   -f $($seleniumContainers)
 
-# docker rm -f \$(docker ps -qa) #remove all when needed
+# docker rm -f $(docker ps -qa) #remove all when needed
---
+++ docker/up.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+SH=$(cd `dirname $BASH_SOURCE`  && pwd)  # get SH aka SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
+
+docker-compose  -f "$SH/docker-compose.yml"  -p 'myseleniumhub'  up  -d           --force-recreate --remove-orphans  # --scale selenium-node-ch=1  --scale selenium-node-ff=0
+#               docker compose file          stack prefix            detach mode  do we need these?                  # scale multiple nodes if needed
+#                                                                                 TODO
