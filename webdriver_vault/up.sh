0927442ab9bfff10beef027e2ec099102267b979
namgivu <namgivu@gmail.com>
2020-06-12 20:52:29 +0700

    .yml not .yaml

--- webdriver_vault/up.sh
+++ webdriver_vault/up.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 SH=`cd $(dirname $BASH_SOURCE) && pwd`
 source "$SH/.config.sh"
-    docker-compose  -f "$SH/docker-compose.yaml"  -p $STACK_NAME             up  -d
+    docker-compose  -f "$SH/docker-compose.yml"  -p $STACK_NAME             up  -d
     #               docker-compose file path      prefix for container name  .   run as daemon
